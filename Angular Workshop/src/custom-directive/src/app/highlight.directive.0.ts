fd3b262e4bcf595ba26c4c4683669bf2a6a2f4e1
Tony Hudson <ngohungphuc95@gmail.com>
2019-04-21 16:00:41 +0700

    update

---
+++ Angular Workshop/src/custom-directive/src/app/highlight.directive.0.ts
@@ -0,0 +1,15 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+  constructor() { }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
