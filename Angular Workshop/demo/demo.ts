830a80275126439b43cd0495db618c3f1aa2a19b
ngohungphuc <ngohungphuc95@gmail.com>
2019-05-07 13:33:03 +0700

    add demo code

---
+++ Angular Workshop/demo/demo.ts
@@ -0,0 +1,37 @@
+let a = 1;
+let a = 1;
+
+const b = 2;
+b = 3;
+
+class One {
+  private readonly data1;
+  /**
+   *
+   */
+  constructor() {
+    this.data1 = 1;
+  }
+}
+
+abstract class People {
+  abstract baseRate: number;
+  abstract workingDay: number;
+
+  abstract calculateSalary();
+}
+
+class Employee extends People {
+  baseRate: number;
+  workingDay: number;
+  /**
+   *
+   */
+  constructor() {
+      super();
+  }
+
+  calculateSalary(): number {
+    return this.baseRate * this.workingDay;
+  }
+}
