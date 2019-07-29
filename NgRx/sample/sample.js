cd7b9e954c54d8b472bce2c5f15292f2ec96a5c9
ngohungphuc <ngohungphuc95@gmail.com>
2019-07-29 22:11:54 +0700

    update

---
+++ NgRx/sample/sample.js
@@ -0,0 +1,36 @@
+var object = {
+  username: "john",
+  email: "john@mail.com"
+};
+
+var array = [
+  {
+    username: "john",
+    email: "john@mail.com"
+  },
+  {
+    username: "john2",
+    email: "john2@mail.com"
+  }
+];
+
+var map = new Map(); //Empty Map
+var map = new Map([[1,2],[2,3]]); // map = {1=>2, 2=>3}
+
+var map = {
+  1: {
+    id: 1,
+    description:
+      "Angular Tutorial For Beginners - Build Your First App - Hello World Step By Step",
+    duration: "4:17",
+    seqNo: 1,
+    courseId: 1
+  },
+  2: {
+    id: 2,
+    description: "Building Your First  Component - Component Composition",
+    duration: "2:07",
+    seqNo: 2,
+    courseId: 1
+  }
+};
